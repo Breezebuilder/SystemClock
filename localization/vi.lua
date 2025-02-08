f02c6cde08546570491718fdbfcd439aaa58bf49
Breezebuilder <bthompsonsmith@gmail.com>
2025-02-08 19:50:16 +1100

    Vietnamese translation by HuyTheLocFixer

---
+++ localization/vi.lua
@@ -0,0 +1,62 @@
+return {
+	['descriptions'] = {
+		['Mod'] = {
+			['SystemClock'] = {
+				name = 'Đồng Hồ Hệ Thống',
+				text = {
+					"{C:white}Xem giờ hiện tại trực tiếp trong game.{}",
+					" ",
+					" ",
+					" ",
+					" ",
+					" ",
+					" ",
+					" ",
+					" ",
+					" ",
+					" ",
+					" ",
+            		"Đừng để rơi vào {C:purple,E:1}Hố Đen{} nhé."
+				}
+			}
+		}
+	},
+	['misc'] = {
+		['dictionary'] = {
+			['sysclock_visibility_setting'] = "Hiện đồng hồ",
+			['sysclock_draggable_setting'] = "Cho phép kéo thả",
+			['sysclock_time_format_setting'] = "Định dạng thời gian",
+			['sysclock_style_setting'] = "Kiểu",
+			['sysclock_colour_setting'] = "Màu",
+			['sysclock_size_setting'] = "Kích thước",
+			['sysclock_x_position_setting'] = "Vị trí X",
+			['sysclock_y_position_setting'] = "Vị trí Y",
+			['sysclock_styles'] = {
+				"Đơn giản",
+				"Dổ bóng",
+				"Trong suốt",
+				"Thêm viền"
+			},
+			['sysclock_colours'] = {
+				"Trắng",
+				"Xám nhạt",
+				"Xám",
+				"Xám đậm",
+				"Đen",
+				"Đỏ",
+				"Cam",
+				"Vàng",
+				"Vàng óng",
+				"Lục",
+				"Lục Lam",
+				"Lam",
+				"Lam Đậm",
+				"Lam Tím",
+				"Tím",
+				"Tím oải hương",
+				"Hồng",
+				"Cầu vồng"
+			}
+		}
+	}
+}
