330f095f2de1800877d92a4eb452e11e738d123c
Breezebuilder <bthompsonsmith@gmail.com>
2025-03-09 23:58:21 +1100

    Add set colour definitions for DYN_UI colors to be used while in main menu

--- src/utilities.lua
+++ src/utilities.lua
@@ -1,5 +1,12 @@
 local utilities = {}
 
+local DYN_UI_INITIAL_COLOURS = {
+	['DYN_UI.MAIN'] = G.C.BLUE,
+	['DYN_UI.DARK'] = mix_colours(G.C.BLUE, G.C.BLACK, 0.4),
+	['DYN_UI.BOSS_MAIN'] = darken(G.C.BLACK, 0.05),
+	['DYN_UI.BOSS_DARK'] = lighten(G.C.BLACK, 0.07)
+}
+
 function utilities.index_of(table, val)
 	if not val then return nil end
 	for i, v in ipairs(table) do
@@ -69,9 +76,15 @@ end
 
 function utilities.get_colour_from_ref(ref)
 	if not ref then return nil end
+	local colour
 
+	if G.STAGE == G.STAGES.MAIN_MENU then
+		colour = DYN_UI_INITIAL_COLOURS[ref]
+		if colour then return colour end
+	end
+
+	colour = G.C
 	local depth = 0
-	local colour = G.C
 	for obj_name in ref:gmatch("[^%.]+") do
 		colour = colour[obj_name]
 		depth = depth + 1
